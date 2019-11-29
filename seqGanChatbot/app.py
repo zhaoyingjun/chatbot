e3a880490591048a9adeca65476277c9ca19c704
Ebenezer Scrooge <1641367382@qq.com>
2019-11-30 00:24:31 +0800

    Fix multiple issues

--- seqGanChatbot/app.py
+++ seqGanChatbot/app.py
@@ -1,27 +1,29 @@
-
-
 # coding=utf-8
 
 from flask import Flask, render_template, request, make_response
 from flask import jsonify
 
 import sys
-import time  
+import time
 import hashlib
 import threading
 import jieba
 import utils.conf as conf
 import gen.generator as generator
 import utils.conf as conf
+
 """
 定义心跳检测函数
 
 """
 
+
 def heartbeat():
-    print (time.strftime('%Y-%m-%d %H:%M:%S - heartbeat', time.localtime(time.time())))
-    timer = threading.Timer(60, heartbeat)
-    timer.start()
+    print(time.strftime('%Y-%m-%d %H:%M:%S - heartbeat', time.localtime(time.time())))
+    timer_instance = threading.Timer(60, heartbeat)
+    timer_instance.start()
+
+
 timer = threading.Timer(60, heartbeat)
 timer.start()
 
@@ -32,29 +34,28 @@ ElementTree在 Python 标准库中有两种实现。
  尽量使用 C 语言实现的那种，因为它速度更快，而且消耗的内存更少
 """
 
+app = Flask(__name__, static_url_path="/static")
 
-app = Flask(__name__,static_url_path="/static") 
 
 @app.route('/message', methods=['POST'])
-
-#"""定义应答函数，用于获取输入信息并返回相应的答案"""
+# """定义应答函数，用于获取输入信息并返回相应的答案"""
 def reply():
-#从请求中获取参数信息
+    # 从请求中获取参数信息
     req_msg = request.form['msg']
-#将语句使用结巴分词进行分词
-    req_msg=" ".join(jieba.cut(req_msg))
-    #调用decode_line对生成回答信息
-    res_msg = execute.decoder_online(sess, conf.gen_config,model, vocab,rev_vocab, req_msg )
-    #res_msg=generator.get_predicted_sentence(sess,req_msg,vocab,model,conf.gen_config.beam_size,conf.gen_config.buckets)
-    #将unk值的词用微笑符号袋贴
+    # 将语句使用结巴分词进行分词
+    req_msg = " ".join(jieba.cut(req_msg))
+    # 调用decode_line对生成回答信息
+    res_msg = execute.decoder_online(sess, conf.gen_config, model, vocab, rev_vocab, req_msg)
+    # res_msg=generator.get_predicted_sentence(sess,req_msg,vocab,model,conf.gen_config.beam_size,conf.gen_config.buckets)
+    # 将unk值的词用微笑符号袋贴
     res_msg = res_msg.replace('_UNK', '^_^')
-    res_msg=res_msg.strip()
-    
+    res_msg = res_msg.strip()
+
     # 如果接受到的内容为空，则给出相应的回复
     if res_msg == ' ':
-      res_msg = '请与我聊聊天吧'
+        res_msg = '请与我聊聊天吧'
 
-    return jsonify( { 'text': res_msg } )
+    return jsonify({'text': res_msg})
 
 
 """
@@ -64,10 +65,11 @@ http://flask.pocoo.org/docs/0.12/api/#module-flask.json
 """
 
 
-
 @app.route("/")
-def index(): 
+def index():
     return render_template("index.html")
+
+
 #
 '''
 初始化seq2seqModel，并进行动作
@@ -75,14 +77,14 @@ def index():
     1. 调用执行器的主程序
     2. 生成一个在线decode进程，来提供在线聊天服务
 '''
-#_________________________________________________________________
+# _________________________________________________________________
 import tensorflow as tf
 import execute
 
 sess = tf.Session()
-sess, model,vocab, rev_vocab = execute.init_session(sess,conf.gen_config)
-#_________________________________________________________________
+sess, model, vocab, rev_vocab = execute.init_session(sess, conf.gen_config)
+# _________________________________________________________________
 
 # 启动APP
-if (__name__ == "__main__"): 
-    app.run(host = '0.0.0.0', port = 8808) 
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8808)
--- seqGanChatbot/utils/data_utils.py
+++ seqGanChatbot/utils/data_utils.py
@@ -44,18 +44,19 @@ UNK_ID = 3
 _WORD_SPLIT = re.compile(b"([.,!?\"':;)(])")
 _DIGIT_RE = re.compile(br"\d")
 
+
 def basic_tokenizer(sentence):
-  """Very basic tokenizer: split the sentence into a list of tokens."""
-  words = []
-  #sentence = tf.compat.as_bytes(sentence)
-  for space_separated_fragment in sentence.strip().split():
-    words.extend(re.split(_WORD_SPLIT, space_separated_fragment))
-  return [w for w in words if w]
+    """Very basic tokenizer: split the sentence into a list of tokens."""
+    words = []
+    # sentence = tf.compat.as_bytes(sentence)
+    for space_separated_fragment in sentence.strip().split():
+        words.extend(_WORD_SPLIT.split(space_separated_fragment))
+    return [w for w in words if w]
 
 
 def create_vocabulary(vocabulary_path, data_path_list, max_vocabulary_size,
                       tokenizer=None, normalize_digits=True):
-  """Create vocabulary file (if it does not exist yet) from disc_data file.
+    """Create vocabulary file (if it does not exist yet) from disc_data file.
 
   Data file is assumed to contain one sentence per line. Each sentence is
   tokenized and digits are normalized (if normalize_digits is set).
@@ -71,35 +72,35 @@ def create_vocabulary(vocabulary_path, data_path_list, max_vocabulary_size,
       if None, basic_tokenizer will be used.
     normalize_digits: Boolean; if true, all digits are replaced by 0s.
   """
-  if not gfile.Exists(vocabulary_path):
-    print("Creating vocabulary %s from disc_data %s" % (vocabulary_path, data_path_list))
-    vocab = {}
-    for data_path in data_path_list:
-        with gfile.GFile(data_path, mode="r") as f:
-          counter = 0
-          for line in f:
-            counter += 1
-            if counter % 100000 == 0:
-              print("  processing line %d" % counter)
-            line = tf.compat.as_bytes(line)
-            tokens = basic_tokenizer(line)
-            for w in tokens:
-              word = _DIGIT_RE.sub(b"0", w) if normalize_digits else w
-              if word in vocab:
-                vocab[word] += 1
-              else:
-                vocab[word] = 1
+    if not gfile.Exists(vocabulary_path):
+        print("Creating vocabulary %s from disc_data %s" % (vocabulary_path, data_path_list))
+        vocab = {}
+        for data_path in data_path_list:
+            with gfile.GFile(data_path, mode="r") as f:
+                counter = 0
+                for line in f:
+                    counter += 1
+                    if counter % 100000 == 0:
+                        print("  processing line %d" % counter)
+                    line = tf.compat.as_bytes(line)
+                    tokens = basic_tokenizer(line)
+                    for w in tokens:
+                        word = _DIGIT_RE.sub(b"0", w) if normalize_digits else w
+                        if word in vocab:
+                            vocab[word] += 1
+                        else:
+                            vocab[word] = 1
 
-    vocab_list = _START_VOCAB + sorted(vocab, key=vocab.get, reverse=True)
-    if len(vocab_list) > max_vocabulary_size:
-      vocab_list = vocab_list[:max_vocabulary_size]
-    with open(vocabulary_path, mode="w") as vocab_file:
-      for w in vocab_list:
-        vocab_file.write(w + "\n")
+        vocab_list = _START_VOCAB + sorted(vocab, key=vocab.get, reverse=True)
+        if len(vocab_list) > max_vocabulary_size:
+            vocab_list = vocab_list[:max_vocabulary_size]
+        with open(vocabulary_path, mode="w") as vocab_file:
+            for w in vocab_list:
+                vocab_file.write(w + "\n")
 
 
 def initialize_vocabulary(vocabulary_path):
-  """Initialize vocabulary from file.
+    """Initialize vocabulary from file.
 
   We assume the vocabulary is stored one-item-per-line, so a file:
     dog
@@ -117,20 +118,20 @@ def initialize_vocabulary(vocabulary_path):
   Raises:
     ValueError: if the provided vocabulary_path does not exist.
   """
-  if gfile.Exists(vocabulary_path):
-    rev_vocab = []
-    with open(vocabulary_path, mode="r") as f:
-      rev_vocab.extend(f.readlines())
-    rev_vocab = [line.strip() for line in rev_vocab]
-    vocab = dict([(x, y) for (y, x) in enumerate(rev_vocab)])
-    return vocab, rev_vocab
-  else:
-    raise ValueError("Vocabulary file %s not found.", vocabulary_path)
+    if gfile.Exists(vocabulary_path):
+        rev_vocab = []
+        with open(vocabulary_path, mode="r") as f:
+            rev_vocab.extend(f.readlines())
+        rev_vocab = [line.strip() for line in rev_vocab]
+        vocab = dict([(x, y) for (y, x) in enumerate(rev_vocab)])
+        return vocab, rev_vocab
+    else:
+        raise ValueError("Vocabulary file %s not found.", vocabulary_path)
 
 
 def sentence_to_token_ids(sentence, vocabulary,
                           tokenizer=None, normalize_digits=True):
-  """Convert a string to list of integers representing token-ids.
+    """Convert a string to list of integers representing token-ids.
 
   For example, a sentence "I have a dog" may become tokenized into
   ["I", "have", "a", "dog"] and with vocabulary {"I": 1, "have": 2,
@@ -147,19 +148,19 @@ def sentence_to_token_ids(sentence, vocabulary,
     a list of integers, the token-ids for the sentence.
   """
 
-  if tokenizer:
-    words = tokenizer(sentence)
-  else:
-    words = basic_tokenizer(sentence)
-  if not normalize_digits:
-    return [vocabulary.get(w, UNK_ID) for w in words]
-  # Normalize digits by 0 before looking words up in the vocabulary.
-  return [vocabulary.get(_DIGIT_RE.sub(b"0", w), UNK_ID) for w in words]
+    if tokenizer:
+        words = tokenizer(sentence)
+    else:
+        words = basic_tokenizer(sentence)
+    if not normalize_digits:
+        return [vocabulary.get(w, UNK_ID) for w in words]
+    # Normalize digits by 0 before looking words up in the vocabulary.
+    return [vocabulary.get(_DIGIT_RE.sub(b"0", w), UNK_ID) for w in words]
 
 
 def data_to_token_ids(data_path, target_path, vocabulary,
                       tokenizer=None, normalize_digits=True):
-  """Tokenize disc_data file and turn into token-ids using given vocabulary file.
+    """Tokenize disc_data file and turn into token-ids using given vocabulary file.
 
   This function loads disc_data line-by-line from data_path, calls the above
   sentence_to_token_ids, and saves the result to target_path. See comment
@@ -173,46 +174,46 @@ def data_to_token_ids(data_path, target_path, vocabulary,
       if None, basic_tokenizer will be used.
     normalize_digits: Boolean; if true, all digits are replaced by 0s.
   """
-  if not gfile.Exists(target_path):
-    print("Tokenizing disc_data in %s" % data_path)
-    #print("target path: ", target_path)
-    #vocab, _ = initialize_vocabulary(vocabulary_path)
-    with gfile.GFile(data_path, mode="r") as data_file:
-      with gfile.GFile(target_path, mode="w") as tokens_file:
-        counter = 0
-        for line in data_file:
-          counter += 1
-          if counter % 100000 == 0:
-            print("  tokenizing line %d" % counter)
-          token_ids = sentence_to_token_ids(line, vocabulary, tokenizer,
-                                            normalize_digits)
-          tokens_file.write(" ".join([str(tok) for tok in token_ids]) + "\n")
+    if not gfile.Exists(target_path):
+        print("Tokenizing disc_data in %s" % data_path)
+        # print("target path: ", target_path)
+        # vocab, _ = initialize_vocabulary(vocabulary_path)
+        with gfile.GFile(data_path, mode="rb") as data_file:
+            with gfile.GFile(target_path, mode="w") as tokens_file:
+                counter = 0
+                for line in data_file:
+                    counter += 1
+                    if counter % 100000 == 0:
+                        print("  tokenizing line %d" % counter)
+                    token_ids = sentence_to_token_ids(line, vocabulary, tokenizer, normalize_digits)
+                    tokens_file.write(" ".join([str(tok) for tok in token_ids]) + "\n")
 
 
 def prepare_chitchat_data(data_dir, vocabulary, vocabulary_size, tokenizer=None):
+    """
   """
-  """
- 
-  train_path = os.path.join(data_dir, "train")
-  #dev_path = get_wmt_enfr_dev_set(data_dir)
-  dev_path = os.path.join(data_dir, "test")
-  
-  answer_train_ids_path = train_path + (".ids%d.answer" % vocabulary_size)
-  query_train_ids_path = train_path + (".ids%d.query" % vocabulary_size)
-  data_to_token_ids(train_path + ".answer", answer_train_ids_path, vocabulary, tokenizer)
-  data_to_token_ids(train_path + ".query", query_train_ids_path, vocabulary, tokenizer)
 
-  # Create token ids for the development disc_data.
-  answer_dev_ids_path = dev_path + (".ids%d.answer" % vocabulary_size)
-  query_dev_ids_path = dev_path + (".ids%d.query" % vocabulary_size)
-  data_to_token_ids(dev_path + ".answer", answer_dev_ids_path, vocabulary, tokenizer)
-  data_to_token_ids(dev_path + ".query", query_dev_ids_path, vocabulary, tokenizer)
+    train_path = os.path.join(data_dir, "train")
+    # dev_path = get_wmt_enfr_dev_set(data_dir)
+    dev_path = os.path.join(data_dir, "test")
 
-  return (query_train_ids_path, answer_train_ids_path,
-          query_dev_ids_path, answer_dev_ids_path)
+    answer_train_ids_path = train_path + (".ids%d.answer" % vocabulary_size)
+    query_train_ids_path = train_path + (".ids%d.query" % vocabulary_size)
+    data_to_token_ids(train_path + ".answer", answer_train_ids_path, vocabulary, tokenizer)
+    data_to_token_ids(train_path + ".query", query_train_ids_path, vocabulary, tokenizer)
+
+    # Create token ids for the development disc_data.
+    answer_dev_ids_path = dev_path + (".ids%d.answer" % vocabulary_size)
+    query_dev_ids_path = dev_path + (".ids%d.query" % vocabulary_size)
+    data_to_token_ids(dev_path + ".answer", answer_dev_ids_path, vocabulary, tokenizer)
+    data_to_token_ids(dev_path + ".query", query_dev_ids_path, vocabulary, tokenizer)
+
+    return (query_train_ids_path, answer_train_ids_path,
+            query_dev_ids_path, answer_dev_ids_path)
+
 
 def hier_prepare_disc_data(data_dir, vocabulary, vocabulary_size, tokenizer=None):
-  """Get WMT disc_data into data_dir, create vocabularies and tokenize disc_data.
+    """Get WMT disc_data into data_dir, create vocabularies and tokenize disc_data.
 
   Args:
     data_dir: directory in which the disc_data sets will be stored.
@@ -230,56 +231,56 @@ def hier_prepare_disc_data(data_dir, vocabulary, vocabulary_size, tokenizer=None
       (5) path to the English vocabulary file,
       (6) path to the French vocabulary file.
   """
-  # Get wmt disc_data to the specified directory.
-  #train_path = get_wmt_enfr_train_set(data_dir)
-  train_path = os.path.join(data_dir, "train")
-  #dev_path = get_wmt_enfr_dev_set(data_dir)
-  dev_path = os.path.join(data_dir, "test")
+    # Get wmt disc_data to the specified directory.
+    # train_path = get_wmt_enfr_train_set(data_dir)
+    train_path = os.path.join(data_dir, "train")
+    # dev_path = get_wmt_enfr_dev_set(data_dir)
+    dev_path = os.path.join(data_dir, "test")
 
-  # Create token ids for the training disc_data.
-  query_train_ids_path = train_path + (".ids%d.query" % vocabulary_size)
-  answer_train_ids_path = train_path + (".ids%d.answer" % vocabulary_size)
-  gen_train_ids_path = train_path + (".ids%d.gen" % vocabulary_size)
+    # Create token ids for the training disc_data.
+    query_train_ids_path = train_path + (".ids%d.query" % vocabulary_size)
+    answer_train_ids_path = train_path + (".ids%d.answer" % vocabulary_size)
+    gen_train_ids_path = train_path + (".ids%d.gen" % vocabulary_size)
 
-  data_to_token_ids(train_path + ".query", query_train_ids_path, vocabulary, tokenizer)
-  data_to_token_ids(train_path + ".answer", answer_train_ids_path, vocabulary, tokenizer)
-  data_to_token_ids(train_path + ".gen", gen_train_ids_path, vocabulary, tokenizer)
+    data_to_token_ids(train_path + ".query", query_train_ids_path, vocabulary, tokenizer)
+    data_to_token_ids(train_path + ".answer", answer_train_ids_path, vocabulary, tokenizer)
+    data_to_token_ids(train_path + ".gen", gen_train_ids_path, vocabulary, tokenizer)
 
-  return (query_train_ids_path, answer_train_ids_path, gen_train_ids_path)
+    return (query_train_ids_path, answer_train_ids_path, gen_train_ids_path)
+
 
 def prepare_disc_data(data_dir, vocabulary, vocabulary_size, tokenizer=None):
+    train_path = os.path.join(data_dir, "train")
+    # dev_path = get_wmt_enfr_dev_set(data_dir)
+    dev_path = os.path.join(data_dir, "test")
 
-  train_path = os.path.join(data_dir, "train")
-  #dev_path = get_wmt_enfr_dev_set(data_dir)
-  dev_path = os.path.join(data_dir, "test")
+    # Create token ids for the training data.
+    answer_train_ids_path = train_path + (".ids%d.pos" % vocabulary_size)
+    query_train_ids_path = train_path + (".ids%d.neg" % vocabulary_size)
+    data_to_token_ids(train_path + ".pos", answer_train_ids_path, vocabulary, tokenizer)
+    data_to_token_ids(train_path + ".neg", query_train_ids_path, vocabulary, tokenizer)
 
-  # Create token ids for the training data.
-  answer_train_ids_path = train_path + (".ids%d.pos" % vocabulary_size)
-  query_train_ids_path = train_path + (".ids%d.neg" % vocabulary_size)
-  data_to_token_ids(train_path + ".pos", answer_train_ids_path, vocabulary, tokenizer)
-  data_to_token_ids(train_path + ".neg", query_train_ids_path, vocabulary, tokenizer)
+    # Create token ids for the development data.
+    answer_dev_ids_path = dev_path + (".ids%d.pos" % vocabulary_size)
+    query_dev_ids_path = dev_path + (".ids%d.neg" % vocabulary_size)
+    data_to_token_ids(dev_path + ".pos", answer_dev_ids_path, vocabulary, tokenizer)
+    data_to_token_ids(dev_path + ".neg", query_dev_ids_path, vocabulary, tokenizer)
 
-  # Create token ids for the development data.
-  answer_dev_ids_path = dev_path + (".ids%d.pos" % vocabulary_size)
-  query_dev_ids_path = dev_path + (".ids%d.neg" % vocabulary_size)
-  data_to_token_ids(dev_path + ".pos", answer_dev_ids_path, vocabulary, tokenizer)
-  data_to_token_ids(dev_path + ".neg", query_dev_ids_path, vocabulary, tokenizer)
-
-  return (query_train_ids_path, answer_train_ids_path,
-          query_dev_ids_path, answer_dev_ids_path)
-
+    return (query_train_ids_path, answer_train_ids_path,
+            query_dev_ids_path, answer_dev_ids_path)
 
 
 def prepare_defined_data(data_path, vocabulary, vocabulary_size, tokenizer=None):
-  #vocab_path = os.path.join(data_dir, "vocab%d.all" %vocabulary_size)
-  #query_vocab_path = os.path.join(data_dir, "vocab%d.query" %query_vocabulary_size)
+    # vocab_path = os.path.join(data_dir, "vocab%d.all" %vocabulary_size)
+    # query_vocab_path = os.path.join(data_dir, "vocab%d.query" %query_vocabulary_size)
 
-  answer_fixed_ids_path = data_path + (".ids%d.answer" % vocabulary_size)
-  query_fixed_ids_path = data_path + (".ids%d.query" % vocabulary_size)
+    answer_fixed_ids_path = data_path + (".ids%d.answer" % vocabulary_size)
+    query_fixed_ids_path = data_path + (".ids%d.query" % vocabulary_size)
 
-  data_to_token_ids(data_path + ".answer", answer_fixed_ids_path, vocabulary, tokenizer)
-  data_to_token_ids(data_path + ".query", query_fixed_ids_path, vocabulary, tokenizer)
-  return (query_fixed_ids_path, answer_fixed_ids_path)
+    data_to_token_ids(data_path + ".answer", answer_fixed_ids_path, vocabulary, tokenizer)
+    data_to_token_ids(data_path + ".query", query_fixed_ids_path, vocabulary, tokenizer)
+    return (query_fixed_ids_path, answer_fixed_ids_path)
+
 
 def get_dummy_set(dummy_path, vocabulary, vocabulary_size, tokenizer=None):
     dummy_ids_path = dummy_path + (".ids%d" % vocabulary_size)
